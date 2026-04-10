6b1464c8406fcacf2d624076d174027978b9971b
stepa323 <voytov.st.vi@gmail.com>
2026-04-10 20:43:02 +0200

    images added

--- lyceum23/settings.py
+++ lyceum23/settings.py
@@ -90,6 +90,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
