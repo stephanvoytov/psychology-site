51b56b6803a7a06a9b5ae7ad12ca524a2616eb79
stepa323 <voytov.st.vi@gmail.com>
2026-04-11 09:42:28 +0200

    testing and fixing shell script

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
-python manage.py makemigrations
+
 python manage.py migrate
+
 python manage.py create_admin
-cd /opt/render/project/src
-source .venv/bin/activate
-python manage.py shell -c "from django.db import connections; print(connections['default'].settings_dict['NAME'])"
-python manage.py shell -c "from django.db import connections; print(connections['default'].settings_dict['HOST'])"
