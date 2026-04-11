41024f20570a8003e527633022268662d40ada0a
stepa323 <voytov.st.vi@gmail.com>
2026-04-11 09:37:48 +0200

    testing

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 python manage.py create_admin
+cd /opt/render/project/src
+source .venv/bin/activate
+python manage.py shell -c "from django.db import connections; print(connections['default'].settings_dict['NAME'])"
+python manage.py shell -c "from django.db import connections; print(connections['default'].settings_dict['HOST'])"
