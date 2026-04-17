9dc629501f7fb04a70aeff345257598b24247414
stepa323 <voytov.st.vi@gmail.com>
2026-04-17 21:54:49 +0200

    email testing

--- lyceum23/urls.py
+++ lyceum23/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.core.mail import send_mail
+from django.http import HttpResponse
 from django.urls import path, include
 
 from django.conf import settings
@@ -7,4 +9,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('booking.urls')),
+    path('test-email/', lambda request: (send_mail('Test', 'Body', 'stepanvoytov@yandex.ru', ['voytov.st.vi@gmail.com']), HttpResponse('Sent')) )
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
