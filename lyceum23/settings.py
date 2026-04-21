b6719f6ab07861c47a088591a00ce0783e98d0aa
stepa323 <voytov.st.vi@gmail.com>
2026-04-21 18:19:34 +0200

    allowed_hosts update

--- lyceum23/settings.py
+++ lyceum23/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '7171562738371671916252781919177151611
 # Режим отладки — только для локальной разработки
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
