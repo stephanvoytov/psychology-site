3ac70eb34f00a35f6b373c408f6300fe4ee4acfc
Stepan Voytov <stepan@voytov.dev>
2026-06-08 20:33:21 +0200

    build: add --system flag to uv pip install

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+pip install uv
+uv pip install --system -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
