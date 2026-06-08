d309688037355ef38e2b7164b823ded26a624201
Stepan Voytov <stepan@voytov.dev>
2026-06-08 20:32:48 +0200

    build: pin Python 3.12.9 via runtime.txt, revert to pip install

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install uv
-uv pip install -r requirements.txt
+pip install -r requirements.txt
 
-uv run python manage.py collectstatic --noinput
-uv run python manage.py migrate --noinput
-uv run python manage.py create_admin
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+python manage.py create_admin
